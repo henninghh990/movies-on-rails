d370a038b17bf624434141083d081e7fc76495e1
henninghh990 <henninghh990@hotmail.com>
2015-08-27 22:46:19 +0200

    Changed search and index for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
 layout "template"
 before_action :authenticate_user!, except: [:show]
   def index
-  	
+    @movies = Movie.all
   end
 
   def show
@@ -17,32 +17,41 @@ before_action :authenticate_user!, except: [:show]
   end
 
   def search
-    if params[:q]
-      if params[:q].blank?
-        flash[:notice] = "No search parameters detected."
-        flash[:class] = "alert-danger"
-        render('index')
+    TMDB::API.api_key = "ce57411404742be5f5ae111fb347f20e"
+    
+      
+        @movies = TMDB::Movie.advanced_search('release_date.gte' => Date.today-1.month,
+                            'release_date.lte' => Date.today)# 'vote_average.gte' => params[:ex1], 'vote_average.lte' => params[:ex2])
+        @tekst = Date.today-1.month
+        @lists = current_user.lists
+        @search_title = "Movies from #{Date.today-1.month} to #{Date.today}"
+      
 
-      else
-      	TMDB::API.api_key = "ce57411404742be5f5ae111fb347f20e"
+      #else
+      if params[:title]
         @movies = TMDB::Movie.search(params[:q])
         @lists = current_user.lists
-        if @movies.nil?
+        @search_title = "Search result for #{params[:q]}"
+        if @movies.blank?
           flash[:notice] = "No movies matches your search"
           flash[:class] = "alert-warning"
           redirect_to(:action => 'index')
         end
-
-      end
     end
 
-    if params[:date_from]
-      (params[:date_from])? date = params[:date_from]: date = DateTime.now 
-      date_from = date.to_a.sort.collect{|c| c[1]}.join("-")
-      (params[:date_to])? date = params[:date_to]: date = DateTime.now 
-      date_to = date.to_a.sort.collect{|c| c[1]}.join("-")
-      @movies = TMDB::Movie.advanced_search('release_date.gte' => '2003',
-                            'release_date.lte' => '2004')# 'vote_average.gte' => params[:ex1], 'vote_average.lte' => params[:ex2])
+    if params[:date]
+      rate = params[:ex1].split(',')
+      date = params[:ex2].split(',')
+      @datelol = "From #{date[0]}-01-01, to #{date[1]}-21-31"
+      @rate_from = rate[0]
+      @rate_to = rate[1]
+      #(params[:date_from])? date = params[:date_from]: date = DateTime.now 
+      #date_from = date.to_a.sort.collect{|c| c[1]}.join("-")
+      #(params[:date_to])? date = params[:date_to]: date = DateTime.now 
+      #date_to = date.to_a.sort.collect{|c| c[1]}.join("-")
+      @movies = TMDB::Movie.advanced_search('release_date.gte' => '#{date[0]}-01-01',
+                            'release_date.lte' => '#{date[1]}-12-31', 'vote_average.gte' => rate[0], 'vote_average.lte' => rate[1])
+      @search_title = "Advanced Search"
       @lists = current_user.lists
       if @movies.blank?
           flash[:notice] = "No movies matches your search"
